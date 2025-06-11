656e60bb362001dff0fe91a6e4ee492c65a42000
Cerovácz Richárd <ageroz13@gmail.com>
2025-06-12 00:07:15 +0200

    webserver berakvan továbbá módosítani kell a html részt!

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WebServer.h>
 
 const char *ssid = "ESP32-Hotspot";
 const char *password = "12345678";
+
+WebServer szero(80);
+
+void handleRoot(){
+  //szero.send(200, "text/html", "<h1>Üdv az ESP32-n!</h1><p>Itt lesz majd a WiFi választó oldal</p>");
+
+  //-----------------------------------------------------------
+  //ebben kehet megadni a weboldal kódját !!!!
+  //1.gombok beolvasása
+  //2.szöveg beolvasás
+  //3. beérkező adatok elmentése és kiírása
+  //-----------------------------------------------------------
+  String html = R"rawliteral(       
+  <h1>Szia!</h1>
+  
+  )rawliteral" ;
+  szero.send(200,"text/html",html);
+
+}
 
 void setup() {
   Serial.begin(115200);
@@ -13,8 +33,13 @@ void setup() {
   Serial.println("Access Point létrehozva!");
   Serial.print("IP címe: ");
   Serial.println(WiFi.softAPIP());
+  szero.on("/",handleRoot);
+  szero.begin();
+  Serial.printf("a webserver elindult");
 }
 
 void loop() {
+szero.handleClient();
+
   // ide jöhet később pl. web server, eszközlista, stb.
 }
